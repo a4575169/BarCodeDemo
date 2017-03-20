a42c8e2ef7402cac86f0a1cc2eb77e52aed5c988
xuchao_git <574981531@qq.com>
2017-03-20 14:36:57 +0800

    修改作者

--- myzxing/src/main/java/com/zxing/activity/CaptureActivity.java
+++ myzxing/src/main/java/com/zxing/activity/CaptureActivity.java
@@ -1,8 +1,5 @@
 package com.zxing.activity;
 
-import java.io.IOException;
-import java.util.Vector;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
@@ -32,8 +29,8 @@ import com.zxing.view.ViewfinderView;
 import java.io.IOException;
 import java.util.Vector;
 /**
- * Initial the camera xxx
- * @author Ryan.Tang
+ * Initial the camera
+ * @author xuchao
  */
 public class CaptureActivity extends Activity implements Callback {
 
